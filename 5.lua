de61df206c41b58f948207726925a383be626f28
Codexbucaccho <nekocyan88@gmail.com>
2024-12-31 18:20:38 +0700

    Update 5.lua

--- 5.lua
+++ 5.lua
@@ -18,7 +18,7 @@ getgenv().Configs = {
         ["Enable"] = false,
         ["When x2 Exp Expired"] = false
     },
-    ["Hop Player Near"] = true,
+    ["Hop Player Near"] = false,
     ["Skull Guitar"] = false, -- Farm SG Last
     ["Find Fruit"] = true, -- Will find 1m+ fruit to UNLOCK SWAN DOOR TO ACCESS THIRD SEA
     ["Cursed Dual Katana"] = false --Farm CDK First
